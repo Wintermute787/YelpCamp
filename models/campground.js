cb2aaedc238e357f97018ff2302c0c8ae60fbeb3
Joe <joebarnes333@gmail.com>
2019-01-23 10:40:28 -0800

    uploaded yelpcamp v1

---
+++ models/campground.js
@@ -0,0 +1,30 @@
+//setup Schema
+var mongoose = require("mongoose");
+
+ 
+var campgroundSchema = new mongoose.Schema({
+   name: String,
+   image: String,
+   description: String,
+   location: String,
+   lat: Number,
+   lng: Number,
+   cost: Number,
+   author: {
+      id: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "User"
+      },
+      username: String
+   },
+   comments: [
+      {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "Comment"
+      }
+   ]
+});
+
+
+ 
+module.exports = mongoose.model("Campground", campgroundSchema);
